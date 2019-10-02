262194162915b49dc7c3060af77f63211d6f765c
Fullstack Developer <delosmusicc@gmail.com>
2019-10-02 03:40:23 -0700

    updating again

--- textme/sms.py
+++ textme/sms.py
@@ -3,8 +3,8 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'ACd827e693bc855daa5113c5d2d4f6c9c3'
-auth_token = 'f564dc265c488481f28b639a039538f9'
+account_sid = ''
+auth_token = ''#inseert authroken
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
